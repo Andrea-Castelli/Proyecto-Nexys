7ce1c5706306ef30f4bd68be8c4f082a84ed9d9c
Andrea-Castelli <andrea.castelli@alumnos.upm.es>
2025-12-31 11:37:16 +0100

    añadir boton

--- registro colores/project_1/project_1.srcs/sources_1/new/TOP.vhd
+++ registro colores/project_1/project_1.srcs/sources_1/new/TOP.vhd
@@ -7,14 +7,17 @@ entity TOP is
  port (
     rst : in std_logic;
     clk : in std_logic;
-    pushbutton : in std_logic;
-    contador : out std_logic_vector(0 TO 3)
+    pushbutton_suma : in std_logic;
+    pushbutton_resta : in std_logic;
+    contador : out std_logic_vector(3 downto 0)
  );
 end TOP;
 
 architecture Behavioral of TOP is
-    signal sync_i: std_logic;
-    signal edge_i: std_logic;
+    signal sync_suma_i: std_logic;
+    signal edge_suma_i: std_logic;
+    signal sync_resta_i: std_logic;
+    signal edge_resta_i: std_logic;
     signal contador_i: std_logic_vector(contador'range);
     
     component SYNC
@@ -33,34 +36,49 @@ architecture Behavioral of TOP is
         );
     end component;
     
-    component registro
+    component REGISTRO
     port(
-        clk     : IN std_logic;
-        CE      : IN std_logic;
-        RST_N   : IN std_logic;
-        CUENTA    : OUT std_logic_vector(3 downto 0)
+        clk          : IN std_logic;
+        CE_SUMA      : IN std_logic;
+        CE_RESTA     : IN std_logic;
+        RST_N        : IN std_logic;
+        CUENTA       : OUT std_logic_vector(3 downto 0)
     );
     end component;
 begin
 
 contador<= contador_i;
-        inst_SYNC: SYNC port map(
-             CLK => CLK,
-             ASYNC_IN => PUSHBUTTON,
-             SYNC_OUT => sync_i
-        );
+inst_SYNC_SUMA: SYNC port map(
+    CLK => CLK,
+    ASYNC_IN => pushbutton_suma,
+    SYNC_OUT => sync_suma_i
+);
+
+inst_SYNC_RESTA: SYNC port map(
+    CLK => CLK,
+    ASYNC_IN => pushbutton_resta,
+    SYNC_OUT => sync_resta_i
+);
         
-        inst_DETECTOR_FLANCO: DETECTOR_FLANCO port map(
-            CLK => CLK,
-            SYNC_IN => sync_i,
-            EDGE =>edge_i
-        );
+
+inst_DETECTOR_FLANCO_SUMA: DETECTOR_FLANCO port map(
+    CLK => CLK,
+    SYNC_IN => sync_suma_i,
+    EDGE =>edge_suma_i
+);
+
+inst_DETECTOR_FLANCO_RESTA: DETECTOR_FLANCO port map(
+    CLK => CLK,
+    SYNC_IN => sync_resta_i,
+    EDGE =>edge_resta_i
+);
         
-        inst_registro: registro PORT MAP (
-           CLK => clk,
-           cuenta => contador_i,
-           CE => sync_i,
-           RST_N => rst
+inst_REGISTRO: REGISTRO PORT MAP (
+    CLK => clk,
+    cuenta => contador_i,
+    CE_SUMA => sync_suma_i,
+    CE_RESTA => sync_resta_i,
+    RST_N => rst
         );
 
 end Behavioral;
