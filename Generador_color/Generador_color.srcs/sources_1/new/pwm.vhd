29f3dcc77746a1ac643401f7e4800f6084634549
Daniel-Felix-Sotomayor <daniel.felix@alumnos.upm.es>
2026-01-11 11:54:32 +0100

    prueba

--- Generador_color/Generador_color.srcs/sources_1/new/pwm.vhd
+++ Generador_color/Generador_color.srcs/sources_1/new/pwm.vhd
@@ -32,12 +32,28 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity pwm is
---  Port ( );
+ port(
+ clk_p : in std_logic;
+ duty  : in integer range 0 to 15;
+ pwm_out: out std_logic
+ );
 end pwm;
 
 architecture Behavioral of pwm is
-
+signal contador : integer range 0 to 15;
 begin
-
-
+process (clk_p)
+begin
+  if rising_edge(clk_p) then
+    if contador = 15 then 
+      contador <= 0;
+    else 
+      contador <= contador + 1;
+    end if;
+    if contador < duty then
+      pwm_out <= '1';
+    else
+      pwm_out <= '0';
+end if;
+end process;
 end Behavioral;
