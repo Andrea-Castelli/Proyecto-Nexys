fb8831ac2e5836cd5c77f3c75bd755faed2ffe6f
Andrea-Castelli <andrea.castelli@alumnos.upm.es>
2026-01-15 09:54:50 +0100

    primera parte lista
    juntar parte de registros y parte de pwm, faltan testbench

---
+++ registro colores/project_1/project_1.srcs/sources_1/new/PWM.vhd
@@ -0,0 +1,61 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 16.12.2025 00:35:41
+-- Design Name: 
+-- Module Name: pwm - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity pwm is
+port(
+clk_p : in std_logic;
+duty : in integer range 0 to 15;
+pwm_out : out std_logic
+);
+end pwm; 
+
+architecture Behavioral of pwm is
+signal contador : integer range 0 to 15;
+begin
+process(clk_p)
+begin
+if rising_edge(clk_p) then
+  if contador = 15 then
+    contador <= 15; --mantener en el maximo
+  else 
+    contador <= contador + 1;
+  end if;
+  if contador < duty then
+    pwm_out <= '1';
+  else
+    pwm_out <= '0';
+  end if;
+end if;
+end process; 
+
+end Behavioral;
