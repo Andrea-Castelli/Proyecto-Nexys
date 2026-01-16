abc05b88c56ee0f8a8529822b0ae54a75769910d
jesusasensiob <jesus.asensiob@gmail.com>
2026-01-16 12:27:42 +0100

    lab_1_led

--- registro colores/project_1/project_1.srcs/sources_1/new/GENERADOR_RGB.vhd
+++ registro colores/project_1/project_1.srcs/sources_1/new/GENERADOR_RGB.vhd
@@ -1,73 +1,41 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 15.01.2026 09:31:42
--- Design Name: 
--- Module Name: GENERADOR_RGB - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL; --para el integer
+use IEEE.NUMERIC_STD.ALL;
 
 entity GENERADOR_RGB is
     Port (
-        clk_placa               : in std_logic;
-        rst_n                   : in std_logic;      
-        boton_suma              : in std_logic; 
-        boton_resta             : in std_logic; 
-        LED                     : out std_logic   
+        clk_placa   : in std_logic;
+        rst_n       : in std_logic; 
+        boton_suma  : in std_logic;
+        boton_resta : in std_logic;
+        LED         : out std_logic
     );
 end GENERADOR_RGB;
 
 architecture Structural of GENERADOR_RGB is
-    signal valor_i : std_logic_vector(3 downto 0); --valor del registro
-
-    component BLOQUE_REGISTRO is
-        port (
-            rst              : in std_logic;
-            clk              : in std_logic;
-            pushbutton_suma  : in std_logic;
-            pushbutton_resta : in std_logic;
-            contador         : out std_logic_vector(3 downto 0)
-        );
-    end component;
-
-    component BLOQUE_PWM is
-        Port (
-            clk    : in std_logic;
-            duty   : in integer range 0 to 15; 
-            brillo : out std_logic
-        );
-    end component;
-
+    signal s_suma_sync, s_resta_sync : std_logic;
+    signal tick_suma, tick_resta     : std_logic;
+    signal valor_cuenta              : std_logic_vector(3 downto 0);
 begin
 
-    inst_registro : BLOQUE_REGISTRO
-        port map (
-            rst              => rst_n,
-            clk              => clk_placa,
-            pushbutton_suma  => boton_suma,
-            pushbutton_resta => boton_resta,
-            contador         => valor_i
-        );
+    -- Instancias respetando tu jerarquía (image_1f53c6.png)
+    sync_s : entity work.SYNC
+        port map (CLK => clk_placa, ASYNC_IN => boton_suma, SYNC_OUT => s_suma_sync);
 
-    inst_pwm : BLOQUE_PWM
-        port map (
-            clk    => clk_placa,           -- Mismo reloj para todo el sistema
-            duty   => to_integer(unsigned(valor_i)), -- convertir a integer
-            brillo => LED              
-        );
+    temp_s : entity work.TEMPORIZADOR_BOTONES
+        port map (clk => clk_placa, rst_n => rst_n, enable => s_suma_sync, tick => tick_suma);
+
+    sync_r : entity work.SYNC
+        port map (CLK => clk_placa, ASYNC_IN => boton_resta, SYNC_OUT => s_resta_sync);
+
+    temp_r : entity work.TEMPORIZADOR_BOTONES
+        port map (clk => clk_placa, rst_n => rst_n, enable => s_resta_sync, tick => tick_resta);
+
+    inst_reg : entity work.registro
+        port map (clk => clk_placa, rst_n => rst_n, CE_SUMA => tick_suma, 
+                  CE_RESTA => tick_resta, cuenta => valor_cuenta);
+
+    inst_pwm : entity work.pwm
+        port map (clk_p => clk_placa, duty => to_integer(unsigned(valor_cuenta)), pwm_out => LED);
 
 end Structural;
--- registro colores/project_1/project_1.srcs/sources_1/new/TOP_PWM.vhd
+++ registro colores/project_1/project_1.srcs/sources_1/new/TOP_PWM.vhd
@@ -1,23 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 14.01.2026 21:37:27
--- Design Name: 
--- Module Name: TOP_PWM - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
